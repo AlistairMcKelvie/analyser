3d9ef410c158058daa38864f150ac25da23c1cc6
Alistair McKelvie <alistair.mckelvie@gmail.com>
2015-03-31 17:16:43 +1100

    initial commit

---
+++ get_color.py
@@ -0,0 +1,81 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from intersect import intersects, intersection_pt, points_in_poly
+from kivy.graphics.vertex_instructions import Line
+from kivy.graphics import Color
+from kivy.graphics.fbo import Fbo
+
+kivy.require('1.8.0')
+
+kv_file = get_color.kv
+
+class Painter(BoxLayout):
+    def __init__(self, **kwargs):
+        super(Painter, self).__init__(**kwargs)
+
+    def on_touch_down(self, touch):
+        self.canvas.clear()
+        self.points_list = []
+        with self.canvas:
+            touch.ud['line'] = Line(points=(touch.x, touch.y), width=3)
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+        self.points_list.append((touch.x, touch.y))
+
+    def return_points(self):
+        intersection_found = False
+        for a in xrange(len(self.points_list)-2):
+            for c in xrange(a + 2, len(self.points_list)-1):
+                b = a + 1
+                d = c + 1
+                seg1 = (self.points_list[a], self.points_list[b])
+                seg2 = (self.points_list[c], self.points_list[d])
+
+                if intersects(seg1, seg2):
+                    intersection_found = True
+                    intersection = intersection_pt(seg1, seg2)
+                    if intersection['point']:
+                        poly_pts = self.points_list[b:c]
+                        print "intersection['point']", intersection['point']
+                        poly_pts.append(intersection['point'])
+                    else:
+                        poly_pts = self.points_list[b:c]
+                        poly_pts.append(intersection['seg'][0])
+                        poly_pts.append(intersection['seg'][1])
+
+        if not intersection_found:
+            poly_pts = self.points_list
+        
+        # make list of tuples in to list as Line() requires
+        poly_pts_list = [item for pair in poly_pts for item in pair]
+
+        # draw shape
+        self.canvas.clear()
+        with self.canvas:
+            Line(points=poly_pts_list, width=3, close=True)
+        #print len(points_in_poly(closed_poly_pts, 400, 700))
+
+    def wipe_line(self):
+        self.canvas.clear()
+
+    def get_pixel_color(self, x, y):
+        image = self.parent.ids['image']
+        fbo = Fbo(texture=image.texture, size=image.texture.size)
+        offset_x = (image.width - image.norm_image_size[0]) / 2
+        offset_y = (image.height - image.norm_image_size[1]) / 2
+        scaled_x = x * (image.texture.width / image.norm_image_size[0])
+        scaled_y = y * (image.texture.height / image.norm_image_size[1])
+        return fbo.get_pixel_color(scaled_x - offset_x, scaled_y - offset_y)
+
+
+class Main(App):
+    def build(self):
+        return Builder.load_fil(get_color.kv)
+
+
+if __name__ == '__main__':
+    Main().run()
---
+++ intersect.py
@@ -0,0 +1,137 @@
+from __future__ import division
+
+def point_on_segment(seg=(), pt=()):
+    pt_x_between = (pt[0] <= max(seg[0][0], seg[1][0]) and
+                    pt[0] >= min(seg[0][0], seg[1][0]))
+    pt_y_between = (pt[1] <= max(seg[0][1], seg[1][1]) and
+                    pt[1] >= min(seg[0][1], seg[1][1]))
+    if pt_x_between and pt_y_between:
+        return True
+    else:
+        return False
+
+
+def orientation(p1=(), p2=(), p3=()):
+    # Get orientation of p3 in relation to vector
+    # p1 -> p2
+    val = ((p2[1] - p1[1]) * (p3[0] - p2[0]) -
+           (p2[0] - p1[0]) * (p3[1] - p2[1]))
+    if val == 0:
+        return 'colinear'
+    elif val > 0:
+        return 'clock'
+    else:
+        return 'anticlock'
+
+
+def intersects(seg1=(), seg2=()):
+    # Get orientations for required for all cases.
+    o1 = orientation(seg1[0], seg1[1], seg2[0])
+    o2 = orientation(seg1[0], seg1[1], seg2[1])
+    o3 = orientation(seg2[0], seg2[1], seg1[0])
+    o4 = orientation(seg2[0], seg2[1], seg1[1])
+
+    # General case, segments cross
+    if o1 != o2 and o3 != o4:
+        return True
+
+    # If general case is not true, segments do not cross,
+    # but one point may lie on on the line/
+    elif o1 == 'colinear' and point_on_segment(seg1, seg2[0]):
+        return True
+    elif o2 == 'colinear' and point_on_segment(seg1, seg2[1]):
+        return True
+    elif o3 == 'colinear' and point_on_segment(seg2, seg1[0]):
+        return True
+    elif o4 == 'colinear' and point_on_segment(seg2, seg1[1]):
+        return True
+    else:
+        return False
+
+
+def intersection_pt(seg1=(), seg2=()):
+    '''This function finds the intersection point of two lines
+    segments, it assumes they intersect and does not check for 
+    intersection. Returns a point unless the segments lie on
+    to of each other, in which case it returns a segment
+    corresponding to their intersection.'''
+    # seg1 is vertical
+    if seg1[0][0] == seg1[1][0]:
+        x = seg1[0][0]
+        # seg2 is also vertical; line are on top each other
+        if seg2[0][0] == seg2[1][0]:
+            # so return a line where they intersect
+            pt_1_y = max(min(seg1[0][1], seg1[1][1]),
+                         min(seg2[0][1], seg2[1][1]))
+            pt_2_y = min(max(seg1[0][1], seg1[1][1]),
+                         max(seg2[0][1], seg2[1][1]))
+            return {'point': None, 'seg': ((x, pt_1_y), (x, pt_2_y))}
+        # seg2 not vertical, so intersect pt exists
+        else:
+            # get equation (y = ax + c) for seg2 and return point
+            # where it crosses seg1
+            a = (seg2[1][1] - seg2[0][1]) / (seg2[1][0] - seg2[0][0])
+            c = seg2[0][1] - a * seg2[0][0]
+            y = a * x + c
+            return {'point': (x, y), 'seg': None}
+    # seg2 is vertical
+    elif seg2[0][0] == seg2[1][0]:
+        x = seg2[0][0]
+        # get equation (y = ax + c) for seg1 and return point
+        # where it crosses seg1
+        a = (seg1[1][1] - seg1[0][1]) / (seg1[1][0] - seg1[0][0])
+        c = seg1[0][1] - a * seg1[0][0]
+        y = a * x + c
+        return {'point': (x, y), 'seg': None}
+    else:
+        # get equations (y = ax + c) for both segs
+        a1 = (seg1[1][1] - seg1[0][1]) / (seg1[1][0] - seg1[0][0])
+        a2 = (seg2[1][1] - seg2[0][1]) / (seg2[1][0] - seg2[0][0])
+        # If gradients are the same lines lie on top of each other
+        if a1 == a2:
+            # so return a line where they intersect
+            pt_1_x = max(min(seg1[0][0], seg1[1][0]),
+                         min(seg2[0][0], seg2[1][0]))
+            pt_2_x = min(max(seg1[0][0], seg1[1][0]),
+                         max(seg2[0][0], seg2[1][0]))
+            pt_1_y = max(min(seg1[0][1], seg1[1][1]),
+                         min(seg2[0][1], seg2[1][1]))
+            pt_2_y = min(max(seg1[0][1], seg1[1][1]),
+                         max(seg2[0][1], seg2[1][1]))
+            return {'point': None, 'seg': ((pt_1_x, pt_1_y), (pt_2_x, pt_2_y))}
+        # lines intersect (general case)
+        else:
+            c1 = seg1[0][1] - a1 * seg1[0][0]
+            c2 = seg2[0][1] - a2 * seg2[0][0]
+            x = (c2 - c1) / (a1 - a2)
+            y = a1 * x + c1
+            return {'point': (x, y), 'seg': None}
+
+def points_in_poly(poly, width, height):
+    '''returns all the points that lie inside a polygon, including those which lie
+    on the boundary. Points are a resolution of one unit, starting from the left 
+    of the polygon.'''
+    # add first point to end to close path
+    poly.append(poly[0])
+    print poly
+    segs_list = []
+    for y in range(height):
+        print 'y', y
+        intersection_points = []
+        for i in range(len(poly) - 1):
+            seg = (poly[i], poly[i + 1])
+            print 'seg', seg
+            #import ipdb; ipdb.set_trace()
+            if intersects(((0, y), (width, y)), seg):
+                print 'found'
+                intersection_points.append(intersection_pt(((0, y), (width, y)), seg))
+        points_in_row = []
+        for intersect in intersection_points:
+            # only use the intersect points, as the segments will have corresponding
+            # points found anyway, don't include existing points
+            if intersect['point'] is not None and intersect['point'] not in points_in_row:
+                points_in_row.append(intersect['point'])
+        points_in_row.sort(key=lambda tup: tup[0])
+        print 'points_in_row', points_in_row
+    return points_in_poly
+
